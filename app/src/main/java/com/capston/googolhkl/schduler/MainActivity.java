b7d3c77af4ca1a0525f584ef116c6f7b12243c5c
olo6525 <googolhkl@gmail.com>
2016-10-28 18:58:10 +0900

    날짜와 요일테두리 자바로 컨트톨

--- app/src/main/java/com/capston/googolhkl/schduler/MainActivity.java
+++ app/src/main/java/com/capston/googolhkl/schduler/MainActivity.java
@@ -2,6 +2,9 @@ package com.capston.googolhkl.schduler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Button mon1 =(Button)findViewById(R.id.mon1);
+        //mon1.setBackgroundResource(R.drawable.button2);
+
+
+        /* 오늘 날짜에 해당하는 요일의 테두리색을 변경 */
+        TextView mon = (TextView)findViewById(R.id.mon);
+        mon.setBackgroundResource(R.drawable.day2);
+
+        /* 현재 시간에 해당하는 시간 테두리색을 변경 */
+        TextView clock = (TextView)findViewById(R.id.clock14);
+        clock.setBackgroundResource(R.drawable.time2);
+
     }
 }
 
